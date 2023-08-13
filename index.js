7f720a83cc719d1c636c461d01541ea665fc25fc
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-08-13 21:29:56 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("The DOM has loaded");
+// })
+
+// console.log("After");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+})
+
